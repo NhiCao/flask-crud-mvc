b77174c6941c3ffbbe2de7611230ed904cff6568
nhihaicao <nhi.cao@dac-datatech.vn>
2022-09-15 17:00:51 +0700

    update

--- tests/test_2.py
+++ tests/test_2.py
@@ -8,14 +8,13 @@ from app import create_app, db
 class TestUser(unittest.TestCase):
     def setUp(self):
         self.app = create_app(config.TestingConfig)
-
-        # User.query.delete()
-
-        user1 = User(id='1', name='Nguyen Thi GitHub Actions Test 1', age='22', address='39 Tran Nhan Tong')
-        user2 = User(id='2', name='Nguyen Thi GitHub Actions Test 2', age='22', address='56 Pham Phu Thu')
-        db.session.add(user1)
-        db.session.add(user2)
-        db.session.commit()
+        with self.app.app_context():
+            # User.query.delete()
+            user1 = User(id='1', name='Nguyen Thi GitHub Actions Test 1', age='22', address='39 Tran Nhan Tong')
+            user2 = User(id='2', name='Nguyen Thi GitHub Actions Test 2', age='22', address='56 Pham Phu Thu')
+            db.session.add(user1)
+            db.session.add(user2)
+            db.session.commit()
 
     def test_get_all_users(self):
         with self.app.app_context():
