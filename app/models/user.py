abc95cad0b361feb959a0d13a7cb64a71e581003
nhihaicao <nhi.cao@dac-datatech.vn>
2022-09-15 09:16:47 +0700

    test

--- app/models/user.py
+++ app/models/user.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from app import db
 
 class User(db.Model):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
