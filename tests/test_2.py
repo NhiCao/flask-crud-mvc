16e56eb03f1b90ce9fdfadd359dd985df88452f7
nhihaicao <nhi.cao@dac-datatech.vn>
2022-09-15 16:55:06 +0700

    update

--- tests/test_2.py
+++ tests/test_2.py
@@ -9,7 +9,7 @@ class TestUser(unittest.TestCase):
     def setUp(self):
         self.app = create_app(config.TestingConfig)
 
-        User.query.delete()
+        # User.query.delete()
 
         user1 = User(id='1', name='Nguyen Thi GitHub Actions Test 1', age='22', address='39 Tran Nhan Tong')
         user2 = User(id='2', name='Nguyen Thi GitHub Actions Test 2', age='22', address='56 Pham Phu Thu')
